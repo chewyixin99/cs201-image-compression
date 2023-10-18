f159c355c8837d89a0f299afb31ed8eef40771a6
Chew Yi Xin <chewyixin1999@gmail.com>
2023-10-18 18:38:27 +0800

    initial haar wavelet helper methods

--- Utility.java
+++ Utility.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Utility {
 
@@ -9,6 +10,7 @@ public class Utility {
         // parameters, and making sure the compressed file
         // gets written into outputFileName
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFileName))) {
+            System.out.println(oos);
             oos.writeObject(pixels);
         }
     }
@@ -28,6 +30,51 @@ public class Utility {
                 throw new IOException("Invalid object type in the input file");
             }
         }
+    }
+
+    // helper functions below
+
+    // 2D wavelet transform
+    public static int[][] haarWaveletTransform2D(int[][] input) {
+        int numRows = input.length;
+        int numCols = input[0].length;
+
+        int[][] output = new int[numRows][numCols];
+
+        // row-wise transformation
+        for (int i = 0; i < numRows; i++) {
+            output[i] = haarWaveletTransform1D(input[i]);
+        }
+        // column-wise transformation on the resulting matrix
+        for (int j = 0; j < numCols; j++) {
+            // the length of the column = numRows
+            int[] column = new int[numRows];
+            for (int i = 0; i < numRows; i++) {
+                // traverse vertically
+                column[i] = output[i][j];
+            }
+            int[] transformedColumn = haarWaveletTransform1D(column);
+            for (int i = 0; i < numRows; i++) {
+                output[i][j] = transformedColumn[i];
+            }
+        }
+
+        return output;
+    }
+
+    // 1D wavelet transform
+    public static int[] haarWaveletTransform1D(int[] input) {
+        int length = input.length;
+        int[] output = new int[length];
+
+        for (int i = 0; i < length / 2; i++) {
+            int sumIndex = 2 * i;
+            int diffIndex = 2 * 1 + 1;
+            output[i] = (input[sumIndex] + input[diffIndex]) / 2;
+            output[i + length / 2] = (input[sumIndex] - input[diffIndex]) / 2;
+        }
+
+        return output;
     }
 
 }
