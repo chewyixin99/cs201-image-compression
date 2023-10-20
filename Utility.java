77815d3fdb2e95122bb63be44ae73a78d3d7cf78
Chew Yi Xin <chewyixin1999@gmail.com>
2023-10-20 18:28:42 +0800

    working encoding, but negative bytes saved

--- Utility.java
+++ Utility.java
@@ -3,10 +3,10 @@ import java.util.*;
 
 public class Utility {
 
+    ContextData contextData;
+
     public void Compress(int[][][] pixels, String outputFileName) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(outputFileName))) {
-            // manipulate pixels (or a copy of) before writing to outputstream
-            // todo: edit here
 
             // wavelet transform step
             int haarWaveletTransformLevels = 1;
@@ -17,38 +17,47 @@ public class Utility {
             int numColors = 64; // set the number of colors in the palette
             int[][][] quantizedRGB = quantizeColors(transformedPixels, numColors);
 
+            // initialize tree
+            HuffmanTree tree = new HuffmanTree();
+            tree.buildHuffmanTree(quantizedRGB);
+
             // encoding step
-            List<Integer> encodedQuantizedRGB = encodeSymbols(quantizedRGB);
-            // todo: end edit here
+            String encodedString = tree.encodeQuantizedRGBToString(quantizedRGB);
+
+            // final step, package necessary data to decode
+            int numRows = quantizedRGB.length;
+            int numCols = quantizedRGB[0].length;
+            int numChannels = quantizedRGB[0][0].length;
+            ContextData data = new ContextData(tree, encodedString, numRows, numCols, numChannels);
+            this.contextData = data;
+
             // write to outputstream and save the file
-            oos.writeObject(quantizedRGB);
-            // oos.writeObject(encodedQuantizedRGB);
+            oos.writeObject(encodedString);
         }
     }
 
     public int[][][] Decompress(String inputFileName) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(inputFileName))) {
             Object object = ois.readObject();
-
-            if (object instanceof List) {
-                // reverse the compression algorithm before returning the original object
-                // manipulate object (or a copy of) before returning
-                // todo: edit here
+            if (object instanceof String) {
+                // retrieve huffman tree
+                HuffmanTree tree = contextData.getHuffmanTree();
+                String encodedString = (String) object;
                 // decoding step
-                List<Integer> encodedQuantizedRGB = convertToIntegerList(Arrays.asList(object));
-                int[][][] decodedQuantizedRGB = decodeSymbols(encodedQuantizedRGB);
+                int[][][] decodedQuantizedRGB = tree.decodeStringToQuantizedRGB(encodedString,
+                        contextData.getNumRows(), contextData.getNumCols(), contextData
+                                .getNumChannels());
 
-                // inverse wavelet transform step
+                // inverse transform step
                 int haarWaveletTransformLevels = 1;
-                int[][][] outputPixels = inverseHaarWaveletTransform3D(decodedQuantizedRGB, haarWaveletTransformLevels);
-                // todo: end edit here
-                return outputPixels;
-            } else if (object instanceof int[][][]) {
-                // todo: delete this block once decoding is implemented
-                int haarWaveletTransformLevels = 1;
-                int[][][] outputPixels = inverseHaarWaveletTransform3D((int[][][]) object, haarWaveletTransformLevels);
-                // todo: end edit here
-                return outputPixels;
+
+                // int[][][] test = inverseHaarWaveletTransform3D(decodedQuantizedRGB,
+                // haarWaveletTransformLevels);
+                // System.out.println(test.length + " " + test[0].length + " " +
+                // test[0][0].length);
+
+                return inverseHaarWaveletTransform3D(decodedQuantizedRGB,
+                        haarWaveletTransformLevels);
             } else {
                 throw new IOException("Invalid object type in the input file");
             }
@@ -178,188 +187,216 @@ public class Utility {
         return (value / step) * step;
     }
 
-    public static List<Integer> encodeSymbols(int[][][] quantizedRGB) {
+    public static Map<List<Integer>, Integer> calculateRGBFrequencies(int[][][] quantizedRGB) {
+        HashMap<List<Integer>, Integer> rgbFrequencies = new HashMap<>();
+
         int numRows = quantizedRGB.length;
         int numCols = quantizedRGB[0].length;
-        int numChannels = quantizedRGB[0][0].length;
-        List<Integer> encodedSymbols = new ArrayList<>();
-        AdaptiveHuffmanTree tree = new AdaptiveHuffmanTree();
 
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                for (int k = 0; k < numChannels; k++) {
-                    int symbol = quantizedRGB[i][j][k];
-                    List<Integer> bits = tree.encodeSymbol(symbol);
-                    encodedSymbols.addAll(bits);
+                List<Integer> currKey = Arrays.stream(quantizedRGB[i][j]).boxed().toList();
+                if (rgbFrequencies.containsKey(currKey)) {
+                    Integer currVal = rgbFrequencies.get(currKey);
+                    rgbFrequencies.put(currKey, currVal + 1);
+                } else {
+                    rgbFrequencies.put(currKey, 1);
                 }
             }
         }
 
-        return encodedSymbols;
-    }
-
-    public static int[][][] decodeSymbols(List<Integer> encodedSymbols) {
-        int numChannels = 3; // Assuming 3 color channels (R, G, B)
-        List<Integer> decodedSymbols = new ArrayList<>();
-        int symbol;
-        AdaptiveHuffmanTree tree = new AdaptiveHuffmanTree();
-
-        int row = 0;
-        int col = 0;
-        int channel = 0;
-
-        while (!encodedSymbols.isEmpty()) {
-            symbol = tree.decodeSymbol(encodedSymbols);
-            decodedSymbols.add(symbol);
-
-            if (channel == numChannels - 1) {
-                channel = 0;
-                col++;
-                if (col == encodedSymbols.size()) {
-                    col = 0;
-                    row++;
-                }
-            } else {
-                channel++;
-            }
-        }
-
-        int numRows = row + 1;
-        int numCols = col + 1;
-        int[][][] decodedRGB = new int[numRows][numCols][numChannels];
-
-        for (int i = 0; i < decodedSymbols.size(); i++) {
-            int value = decodedSymbols.get(i);
-            int yi = i / (numCols * numChannels);
-            int xi = (i / numChannels) % numCols;
-            int ci = i % numChannels;
-            decodedRGB[yi][xi][ci] = value;
-        }
-
-        return decodedRGB;
-    }
-
-    public static List<Integer> convertToIntegerList(List<Object> inputList) {
-        List<Integer> output = new ArrayList<>();
-        for (Object item : inputList) {
-            output.add((Integer) item);
-        }
-        return output;
+        return rgbFrequencies;
     }
 
 }
 
-class AdaptiveHuffmanTree {
-    private AdaptiveHuffmanNode root;
-    private AdaptiveHuffmanNode NYT; // Not Yet Transmitted node
+class HuffmanNode implements Comparable<HuffmanNode>, Serializable {
+    int frequency;
+    List<Integer> value;
+    HuffmanNode left;
+    HuffmanNode right;
 
-    public AdaptiveHuffmanTree() {
-        root = new AdaptiveHuffmanNode();
-        NYT = root;
-    }
-
-    public List<Integer> encodeSymbol(int symbol) {
-        List<Integer> bits = new ArrayList<>();
-        AdaptiveHuffmanNode node = findNodeBySymbol(symbol);
-
-        if (node == null) {
-            bits.addAll(encodeSymbol(NYT, symbol));
-            updateTree(symbol);
-        } else {
-            bits.addAll(encodeSymbol(node, symbol));
-            updateTree(node);
-        }
-
-        return bits;
-    }
-
-    public int decodeSymbol(List<Integer> bits) {
-        AdaptiveHuffmanNode node = root;
-
-        while (true) {
-            if (node.isLeaf()) {
-                return node.symbol;
-            }
-
-            int bit = bits.remove(0);
-            node = (bit == 0) ? node.left : node.right;
-        }
-    }
-
-    private List<Integer> encodeSymbol(AdaptiveHuffmanNode node, int symbol) {
-        List<Integer> bits = new ArrayList<>();
-
-        while (node != root) {
-            if (node.isNYT()) {
-                bits.add(0); // 0 represents NYT
-                bits.addAll(toBinary(symbol, 8)); // Assuming 8-bit symbols
-                return bits;
-            }
-
-            if (node.isLeftChild()) {
-                bits.add(0);
-            } else {
-                bits.add(1);
-            }
-
-            node = node.parent;
-        }
-
-        return bits;
-    }
-
-    private List<Integer> toBinary(int value, int numBits) {
-        List<Integer> bits = new ArrayList<>();
-        for (int i = 0; i < numBits; i++) {
-            bits.add((value >> (numBits - i - 1)) & 1);
-        }
-        return bits;
-    }
-
-    // todo: implement the following 3 methods for encoding and decoding
-
-    private AdaptiveHuffmanNode findNodeBySymbol(int symbol) {
-        // Implement search for the node containing the symbol
-        // Return null if not found
-        // You may need to traverse the tree
-        return null;
-    }
-
-    private void updateTree(int symbol) {
-        // Implement tree update logic to accommodate the new symbol
-        // You will need to adjust node weights and tree structure
-    }
-
-    private void updateTree(AdaptiveHuffmanNode node) {
-        // Implement tree update logic to accommodate the recently transmitted node
-        // You will need to adjust node weights and tree structure
-    }
-}
-
-class AdaptiveHuffmanNode {
-    int weight;
-    int symbol;
-    AdaptiveHuffmanNode parent;
-    AdaptiveHuffmanNode left;
-    AdaptiveHuffmanNode right;
-
-    public AdaptiveHuffmanNode() {
-        weight = 0;
-        symbol = -1;
-        parent = null;
+    public HuffmanNode(List<Integer> value, int frequency) {
+        this.value = value;
+        this.frequency = frequency;
         left = null;
         right = null;
     }
 
-    public boolean isLeaf() {
-        return left == null && right == null;
+    @Override
+    public int compareTo(HuffmanNode other) {
+        return this.frequency - other.frequency;
     }
 
-    public boolean isNYT() {
-        return symbol == -1;
+    public boolean equals(HuffmanNode other) {
+        return this.frequency == other.frequency;
+    }
+}
+
+class HuffmanTree implements Serializable {
+
+    private Map<List<Integer>, String> huffmanCodes;
+    private HuffmanNode rootNode;
+
+    public HuffmanTree() {
+
     }
 
-    public boolean isLeftChild() {
-        return parent != null && parent.left == this;
+    public Map<List<Integer>, String> getHuffmanCodes() {
+        return this.huffmanCodes;
     }
+
+    public HuffmanNode getRootNode() {
+        return this.rootNode;
+    }
+
+    public void buildHuffmanTree(int[][][] quantizedRGB) {
+        PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>();
+        Map<List<Integer>, String> huffmanCodes = new HashMap<>();
+
+        Map<List<Integer>, Integer> rgbFrequencies = calculateRGBFrequencies(quantizedRGB);
+
+        // Create a leaf node for each unique RGB value and add it to the priority
+        // queue.
+        for (Map.Entry<List<Integer>, Integer> entry : rgbFrequencies.entrySet()) {
+            HuffmanNode node = new HuffmanNode(entry.getKey(), entry.getValue());
+            priorityQueue.add(node);
+        }
+
+        // Build the Huffman tree
+        while (priorityQueue.size() > 1) {
+            HuffmanNode left = priorityQueue.poll();
+            HuffmanNode right = priorityQueue.poll();
+
+            HuffmanNode parent = new HuffmanNode(null, left.frequency + right.frequency);
+            parent.left = left;
+            parent.right = right;
+
+            priorityQueue.add(parent);
+        }
+
+        // Last remaining element becomes the root
+        if (!priorityQueue.isEmpty()) {
+            HuffmanNode root = priorityQueue.peek();
+            generateHuffmanCodes(root, "", huffmanCodes);
+        }
+
+        this.huffmanCodes = huffmanCodes;
+        this.rootNode = priorityQueue.peek();
+    }
+
+    private static void generateHuffmanCodes(HuffmanNode node, String code, Map<List<Integer>, String> huffmanCodes) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.value != null) {
+            huffmanCodes.put(node.value, code);
+        }
+
+        generateHuffmanCodes(node.left, code + "0", huffmanCodes);
+        generateHuffmanCodes(node.right, code + "1", huffmanCodes);
+    }
+
+    public String encodeQuantizedRGBToString(int[][][] quantizedRGB) {
+        int numRows = quantizedRGB.length;
+        int numCols = quantizedRGB[0].length;
+        StringBuilder encodedString = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                int[] rgbArr = quantizedRGB[i][j];
+                List<Integer> rgbList = Arrays.stream(rgbArr).boxed().toList();
+                String code = this.huffmanCodes.get(rgbList);
+                encodedString.append(code);
+            }
+        }
+        return encodedString.toString();
+    }
+
+    public int[][][] decodeStringToQuantizedRGB(String encodedString, int numRows, int numCols, int numChannels) {
+        int[][][] decodedArr = new int[numRows][numCols][numChannels];
+        HuffmanNode currentNode = rootNode;
+        int rowIdx = 0;
+        int colIdx = 0;
+
+        for (char bit : encodedString.toCharArray()) {
+            if (bit == '0') {
+                currentNode = currentNode.left;
+            } else if (bit == '1') {
+                currentNode = currentNode.right;
+            }
+
+            if (currentNode.value != null) {
+                int[] valArr = currentNode.value.stream().mapToInt(Integer::intValue).toArray();
+                decodedArr[rowIdx][colIdx] = valArr;
+                colIdx = colIdx + 1;
+                if (colIdx == numCols) {
+                    colIdx = 0;
+                    rowIdx = rowIdx + 1;
+                }
+                currentNode = rootNode;
+            }
+        }
+
+        return decodedArr;
+    }
+
+    public static Map<List<Integer>, Integer> calculateRGBFrequencies(int[][][] quantizedRGB) {
+        HashMap<List<Integer>, Integer> rgbFrequencies = new HashMap<>();
+
+        int numRows = quantizedRGB.length;
+        int numCols = quantizedRGB[0].length;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                List<Integer> currKey = Arrays.stream(quantizedRGB[i][j]).boxed().toList();
+                if (rgbFrequencies.containsKey(currKey)) {
+                    Integer currVal = rgbFrequencies.get(currKey);
+                    rgbFrequencies.put(currKey, currVal + 1);
+                } else {
+                    rgbFrequencies.put(currKey, 1);
+                }
+            }
+        }
+
+        return rgbFrequencies;
+    }
+}
+
+class ContextData implements Serializable {
+    private HuffmanTree huffmanTree;
+    private String encodedString;
+    private int numRows;
+    private int numCols;
+    private int numChannels;
+
+    public ContextData(HuffmanTree huffmanTree, String encodedString, int numRows, int numCols,
+            int numChannels) {
+        this.huffmanTree = huffmanTree;
+        this.encodedString = encodedString;
+        this.numRows = numRows;
+        this.numCols = numCols;
+        this.numChannels = numChannels;
+    }
+
+    public HuffmanTree getHuffmanTree() {
+        return this.huffmanTree;
+    }
+
+    public String getEncodedString() {
+        return this.encodedString;
+    }
+
+    public int getNumRows() {
+        return this.numRows;
+    }
+
+    public int getNumCols() {
+        return this.numCols;
+    }
+
+    public int getNumChannels() {
+        return this.numChannels;
+    }
+
 }
